9b4af9655e593fd9a1175bf9471e4ccfb87830a9
Johnathan Thao <johnathanthao21@gmail.com>
2022-07-08 19:40:53 -0600

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+  const todoForm = document.getElementById('create-task-form');
+  todoForm.addEventListener('submit', handleSubmit)
+  
+  
+  });
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('submitted')
+   console.log (event.target.description.value)
+  
+  const taskList = document.getElementById('tasks');
+  const task = document.createElement('li');
+  task.textContent = event.target.description.value;
+  taskList.append(task);
+  
+  const deleteButton = document.createElement('button')
+  deleteButton.textContent = 'X'
+  deleteButton.addEventListener('click' , () => {
+    taskList.removeChild(task);
+  });
+  
+  
+  task.append(deleteButton)
+  
+  event.target.reset();
+  
+  
+  }
